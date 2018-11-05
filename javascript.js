09967868087ab7bfb1910ce0e814fd52b011cb3e
Alods62 <42915139+Alods62@users.noreply.github.com>
2018-11-05 14:10:30 +0100

    Ajouts des fichiers

---
+++ javascript.js
@@ -0,0 +1,69 @@
+var c = document.getElementById("mon_canvas"); // On récupère l'élément canvas de la page html
+var ctx = c.getContext("2d"); //Définition du contexte 2D ou 3D
+
+
+// EXERCICE 1
+
+// Murs
+ctx.beginPath();      // Début du chemin
+ctx.moveTo(50,150);    // Le tracé part du point 50,150
+ctx.lineTo(200,150);  // Un segment est ajouté vers 200,150
+ctx.lineTo(200,300);
+ctx.lineTo(50,300);
+ctx.lineTo(50,150);
+ctx.closePath(); // Fermeture du tracé (pas obligatoire)
+ctx.fillStyle = "lightblue"; // Choix de la couleur de remplissage
+ctx.fill(); // Remplissage
+
+//Toit
+ctx.beginPath();
+ctx.moveTo(50,150);
+ctx.lineTo(125,50);
+ctx.lineTo(200,150);
+ctx.fillStyle = "peru";
+ctx.fill();
+
+
+
+// EXERCICE 2
+
+// Toit
+ctx.save(); // Sauvegarde la matrice 0,0
+ctx.beginPath();
+ctx.translate( 475, 225); // Déplace la matrice sur une autre position
+ctx.rotate(45*Math.PI/180); // Rotation d'angle 45 degrés
+ctx.rect(-106,-106,105,105);
+ctx.fillStyle = "peru";
+ctx.fill();
+ctx.restore(); // Réinitialise la matrice
+
+
+// Murs
+ctx.beginPath();
+ctx.rect(400,150,150,150);
+ctx.fillStyle ="lightblue";
+ctx.fill();
+
+
+// EXERCICE 3
+
+// Carlingue
+ctx.beginPath();
+ctx.rect(700, 150, 350, 75);
+ctx.fillStyle="lightgreen";
+ctx.fill();
+ctx.rect(750, 100, 250, 75);
+ctx.fill();
+
+// Roues
+ctx.beginPath();
+ctx.arc(740, 265, 40, 0 , Math.PI*2,false); // arc( x, y, radius, startAngle, endAngle, sensAntiHoraire )
+ctx.strokeStyle = "black";
+ctx.stroke();
+ctx.fillStyle = "lightgray";
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(1010, 265, 40, 0 , Math.PI*2,false);
+ctx.stroke();
+ctx.fill();
